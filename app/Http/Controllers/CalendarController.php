d0c84f5addb1b6f34f6f2b8a0a0b0ec174f6450c
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2019-12-26 11:03:39 +0900

    viewファイルのパスを追加

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class CalendarController extends BaseController
+{
+    public function show($user_code)
+    {
+        return view('calendar/show');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/calendar/{user_code}', 'CalendarController@show');
