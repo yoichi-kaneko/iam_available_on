961ba1c408cf9075d6cf0adece9edeaa07ceb2c7
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2020-01-06 14:24:43 +0900

    composerでライブラリ追加、モデルクラスのリレーション追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\UserSetting;
 
 class User extends Authenticatable
 {
@@ -27,6 +28,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setting()
+    {
+        return $this->hasOne('App\Models\UserSetting');
+    }
 
     public static function createByEmail($email)
     {
--- app/Models/UserSetting.php
+++ app/Models/UserSetting.php
@@ -3,16 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class UserSetting extends Model
 {
     protected $fillable = [
         'user_id',
+        'user_code',
         'display_name',
         'weekday_default_status',
         'holiday_default_status',
         'description',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 
     public static function createSetting($user_id, $settings)
     {
@@ -21,4 +28,7 @@ class UserSetting extends Model
             array_merge(['user_id' => $user_id], $settings)
         );
     }
+
+    private static function 
+
 }
