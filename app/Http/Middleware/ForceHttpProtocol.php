6bd00b283d8d848c10841eb55099129b6be82841
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2020-03-14 15:04:00 +0900

    http転送を実装

---
+++ app/Http/Middleware/ForceHttpProtocol.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ForceHttpProtocol {
+
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && env('APP_ENV') === 'production') { // 本番環境のみ常時SSL化する
+            if (!empty($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+                return redirect()->secure($request->getRequestUri());
+            }
+        }
+
+        return $next($request);
+    }
+
+}
