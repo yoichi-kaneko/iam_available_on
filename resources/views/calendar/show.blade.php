b8b542abf92c1ea7aef74ea6fb5eb90bf693fec2
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2020-06-13 12:05:19 +0900

    説明文の調整

--- resources/views/calendar/show.blade.php
+++ resources/views/calendar/show.blade.php
@@ -12,32 +12,45 @@
         <div class="container-fluid">
             <div class="block-header">
                 <div class="row">
-                    <div class="col-lg-12 col-md-6 col-sm-7">
-                        <h2>
-                            @if (!empty($user_info['is_owner']))
-                                {{ $user_info['display_name'] }}さん（あなた）のカレンダー
-                            @else
-                                {{ $user_info['display_name'] }}さんのカレンダー
+                    <div class="col-lg-12 card">
+                        <div class="header">
+                            <h2>
+                                @if (!empty($user_info['is_owner']))
+                                    {{ $user_info['display_name'] }}さん（あなた）のカレンダー
+                                @else
+                                    {{ $user_info['display_name'] }}さんのカレンダー
+                                @endif
+                            </h2>
+                            @if (empty($user_info['is_owner']))
+                            <ul class="header-dropdown m-r--5" style="left: 270px;">
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        <i class="zmdi zmdi-help-outline"></i>
+                                    </a>
+                                    <ul class="dropdown-menu pull-right" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 33px, 0px); top: 0px; left: 0px; will-change: transform;">
+                                        <li style="width:300px;">
+                                            <span style="font-size: 9pt;">
+                                                このページはスケジュール情報の共有専用ページです。相手方への連絡が必要な場合には個別の手段にてお願いします。
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
                             @endif
-                        </h2>
-                        @if (!empty($user_info['is_owner']))
-                            <p>
-                                <div class="line-it-button" data-lang="ja" data-type="share-a" data-ver="3" data-url="{{ url()->current() }}" data-color="default" data-size="small" data-count="false" style="display: none;"></div>
-                                <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+                        </div>
+                        <div class="body">
+                            <p class="margin-0">
+                                {!! nl2br(e($user_info['description'])) !!}
+                                @if (!empty($user_info['is_owner']))
+                                    <div class="line-it-button" data-lang="ja" data-type="share-a" data-ver="3" data-url="{{ url()->current() }}" data-color="default" data-size="small" data-count="false" style="display: none;"></div>
+                                    <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+                                @endif
                             </p>
-                        @endif
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 ">
-                    <div class="m-b-20 card">
-                        <div class="body">
-                            <p>{!! nl2br(e($user_info['description'])) !!}</p>
-
-                        </div>
-                    </div>
-                </div>
                 <div class="col-lg-9 col-md-12">
                     <div class="card" id="calendar_card">
                         <div class="body">
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -30,52 +30,53 @@
                                 @endforeach
                             @endif
                             <form id="user_form" action="{{ action('RegisterController@post') }}" method="POST">
-                            <h2 class="card-inside-title">メールアドレス（変更不可）</h2>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <div class="form-line disabled">
-                                        <input type="text" class="form-control" placeholder="{{ $input_data['email'] }}" disabled="">
+                                <h2 class="card-inside-title">メールアドレス（変更不可）</h2>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <div class="form-line disabled">
+                                            <input type="text" class="form-control" placeholder="{{ $input_data['email'] }}" disabled="">
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <h2 class="card-inside-title">表示名</h2>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input name="display_name" type="text" class="form-control" placeholder="16字以内" value="{{ $input_data['display_name'] }}">
+                                <h2 class="card-inside-title">表示名</h2>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input name="display_name" type="text" class="form-control" placeholder="16字以内" value="{{ $input_data['display_name'] }}">
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <h2 class="card-inside-title">
-                                平日のデフォルト予定
-                                <small>平日にデフォルトでセットされるマークを選択してください。</small>
-                            </h2>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    @include('form/status',['prefix' => 'weekday_default_', 'value' => $input_data['weekday_default_status']])
-                                </div>
-                            </div>
-                            <h2 class="card-inside-title">
-                                土日祝日のデフォルト予定
-                                <small>土日祝日にデフォルトでセットされるマークを選択してください。</small>
-                            </h2>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    @include('form/status',['prefix' => 'holiday_default_', 'value' => $input_data['holiday_default_status']])
-                                </div>
-                            </div>
-                            <h2 class="card-inside-title">表示テキスト</h2>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <textarea name="description" rows="1" class="form-control no-resize auto-growth" placeholder="128字以内">{{ $input_data['description'] }}</textarea>
+                                <h2 class="card-inside-title">
+                                    平日のデフォルト予定
+                                    <small>平日にデフォルトでセットされるマークを選択してください。</small>
+                                </h2>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        @include('form/status',['prefix' => 'weekday_default_', 'value' => $input_data['weekday_default_status']])
                                     </div>
                                 </div>
-                            </div>
-                            {{ csrf_field() }}
+                                <h2 class="card-inside-title">
+                                    土日祝日のデフォルト予定
+                                    <small>土日祝日にデフォルトでセットされるマークを選択してください。</small>
+                                </h2>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        @include('form/status',['prefix' => 'holiday_default_', 'value' => $input_data['holiday_default_status']])
+                                    </div>
+                                </div>
+                                <h2 class="card-inside-title">表示テキスト</h2>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <textarea name="description" rows="1" class="form-control no-resize auto-growth" placeholder="128字以内">{{ $input_data['description'] }}</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                {{ csrf_field() }}
                                 <input type="hidden" name="email" value="{{ $input_data['email'] }}" >
                                 <input type="hidden" name="encrypted" value="{{ $input_data['encrypted'] }}" >
-                            <button class="btn btn-raised btn-primary waves-effect" type="submit">登録する</button>
+                                <button class="btn btn-raised btn-primary waves-effect" type="submit">登録する</button>
+                            </form>
                         </div>
                     </div>
                 </div>
